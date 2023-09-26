498dd4cfde423865ee33ffcd8cf396eccd54abd3
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-09-26 14:27:46 +0530

    Fixed more styles

--- src/containers/Demo/Demo.js
+++ src/containers/Demo/Demo.js
@@ -3,18 +3,20 @@ import './Demo.css'
 
 function Demo() {
   return (
-      <div className='demo'>
-        <div className="demo-left" >
-            <h1>Ready to Transform Your Creative Journey?</h1>
-            <p className='font2'>You've seen what we can offer. Now it's time to make your choice. Don't settle for less when you can have the best.</p>
-            <button className='demo-button'>Get a demo</button>
-        </div>
-        <div className='demo-right' >
-            <div className='demo-img'>
+    <div className='demo'>
+      <div className="demo-left" >
+        <h1>Ready to Transform Your Creative Journey?</h1>
+        <p className='font2'>You've seen what we can offer. Now it's time to make your choice. Don't settle for less when you can have the best.</p>
+        <button className='demo-button'>Get a demo</button>
+      </div>
+      <div className='demo-right-outer'>
+        <div className='demo-right-inner' >
+          <div className='demo-img'>
             <img src='/assets/images4.png'></img>
-            </div>
+          </div>
         </div>
       </div>
+    </div>
   )
 }
 
