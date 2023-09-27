4633b70e9300eab6055e9a0a7094db4434d8951f
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-09-28 02:03:03 +0530

    Made form

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -3,36 +3,45 @@ import { Button, Form, Input, Radio } from 'antd';
 import './Form.css'
 
 const DemoForm = () => {
-    const [form] = Form.useForm();
 
     return (
-        <Form className='demoform'
-            layout={'vertical'}
-            form={form}
-            initialValues={{
-                layout: 'vertical',
-            }}
-            style={{
-                maxWidth: 600,
-            }}
-            labelFontSize={18}
-        >
-            <Form.Item label="Name"  >
-                <Input placeholder="Enter Full Name" />
-            </Form.Item>
-            <Form.Item label="Company Name" >
-                <Input placeholder="Your Company"  />
-            </Form.Item>
-            <Form.Item label="Role">
-                <Input placeholder="Your Role" />
-            </Form.Item>
-            <Form.Item label="Industry">
-                <Input placeholder="Your Current Industry" />
-            </Form.Item>
-            <Form.Item >
-                <button>Submit</button>
-            </Form.Item>
-        </Form>
-    );
+
+        <div class="outer-body">
+            <div class="inner-body">
+
+                <div class="form-header">
+                    <h1>Contact Us</h1>
+                </div>
+
+                <div class="form-body">
+                    <form action="">
+                        <p>
+                            <label for="Name">Name</label>
+                            <input type="text" placeholder="Name" required />
+                        </p>
+
+                        <p>
+                            <label for="Email">Company</label>
+                            <input type="text" placeholder="Company" required />
+                        </p>
+
+                        <p>
+                            <label for="number">Industry</label>
+                            <input type="text" placeholder="Industry" required />
+                        </p>
+                        <p>
+                            <label for="password">Role</label>
+                            <input type="text" placeholder="Role" required />
+                        </p>
+
+                        <p>
+                            <input type="submit" id="submit" value="Submit" />
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>)
+
 };
+
 export default DemoForm;
