3c502fe03fc13a9ec6efa4cecc216e7d46dba174
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-09-28 19:48:53 +0530

    Fixed navbar

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -5,24 +5,35 @@ import { Button, Tooltip } from 'antd';
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-    const navigate= useNavigate();
+    const navigate = useNavigate();
     return (
         <nav className='navbar'>
             <div className='navbar-logo '>
                 <img src="/assets/logo.png" width={28} height={28}></img>
-                <div className='cup navbar-heading' onClick={()=>{navigate('/')}}>SeiSei.ai</div>
+                <div className='cup navbar-heading' onClick={() => { navigate('/') }}>SeiSei.ai</div>
             </div>
             <div className='navbar-section font2'>
-                    <Tooltip placement="bottom" title={
+                <Tooltip placement="bottom" title={
                     <div className='usecase-tooltip'>
-                        <p className='cup' onClick={()=>{navigate('/usecase/marketing')}}>Marketing Campaigns</p>
-                        <p className='cup' onClick={()=>{navigate('/usecase/d2c')}}>D2C eCommerce</p>
-                        <p className='cup' onClick={()=>{navigate('/usecase/stakeholder')}}>Stakeholder Communication</p>
+                        <p className='cup' onClick={() => { navigate('/usecase/marketing') }}>Marketing Campaigns</p>
+                        <p className='cup' onClick={() => { navigate('/usecase/d2c') }}>D2C eCommerce</p>
+                        <p className='cup' onClick={() => { navigate('/usecase/stakeholder') }}>Stakeholder Communication</p>
                     </div>}
                     color='white' arrow={false}>
                     <h1 className='cup'>Use Case</h1>
-                    </Tooltip>
-                <h1 className='cup'>Resources</h1>
+                </Tooltip>
+
+                <Tooltip placement="bottom" title={
+                    <div className='usecase-tooltip-1'>
+                        <p className='cup' onClick={() => { navigate('/ethics') }}>Ethics</p>
+                        <p className='cup' onClick={() => { navigate('/')}}><a href='#pricing'>Pricing</a></p>
+                        <p className=''>Blogs</p>
+                        <p style={{fontSize:'10px'}}>Coming Soon</p>
+                    </div>}
+                    color='white' arrow={false}>
+                    <h1 className='cup'>Resources</h1>
+                </Tooltip>
+
             </div>
             <div className='navbar-section font2'>
                 <div className='language-logo cup'><IoLanguageOutline style={{ width: '32px', height: '32px', color: '#357E5E', fontWeight: '700' }} /></div>
