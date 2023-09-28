fb4138ff62dde91174f544d81c7454e90a37f042
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-09-28 19:32:46 +0530

    Fixed spacing

--- src/containers/Careers/Careers.js
+++ src/containers/Careers/Careers.js
@@ -4,8 +4,13 @@ import './Careers.css'
 function Careers() {
   return (
     <div className='careers'>
-      <p>We are always looking for individuals who are passionate about working in the generative AI space, currently, we are hiring AI ML Engineers and Interns with few years of experience, ownership is the deciding factor for us. Just saying it out loud, skills and proficiency matter to us more than a college degree.
-          <b> Send your resume to <a href='mailto:rajat@seisei.ai'>rajat@seisei.ai</a></b></p>
+      <p>We are always looking for individuals who are passionate about working in the generative AI space, currently, we are hiring AI ML Engineers and Interns with few years of experience, ownership is the deciding factor for us.
+        <br/>
+        <br/> 
+        Just saying it out loud, skills and proficiency matter to us more than a college degree.
+        <br/>
+        <br/>
+           Send your resume to <a href='mailto:rajat@seisei.ai'><b>rajat@seisei.ai</b></a></p>
     </div>
   )
 }
--- src/containers/ContactUs/ContactUs.js
+++ src/containers/ContactUs/ContactUs.js
@@ -7,7 +7,10 @@ function ContactUs() {
       <h1>Customer Support</h1>
       <br />
       <p>If you need assistance with your STUDIO, have a question about our products or services, want to submit a request, or would like to discuss your account, please contact our customer support team via Email.
+        <br/>
+        <br/>
         Opening hours: Daily 9am - 7pm (IST)
+        <br/>
         Contacting Support: email us at support@seisei.ai</p>
       <div className='input-container'>
         <input className='customer-input' placeholder='Email'></input>
